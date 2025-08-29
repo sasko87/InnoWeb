8475eb725a35fa799bb2fe57ec14247eeff5076b
ketibm <k.matenicharska@outlook.com>
2025-08-29 10:31:04 +0200

    project

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { motion } from "framer-motion";
+import classes from "./Projects.module.css";
 
 const Projects = () => {
-  return <div></div>;
+  return (
+    <div className={classes.projectsContainer}>
+      <motion.h2
+        className={classes.subTitle}
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
+        Our Projects
+      </motion.h2>
+    </div>
+  );
 };
 
 export default Projects;
