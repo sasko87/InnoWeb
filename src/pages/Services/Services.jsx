dcfe3bff6ceb7a076ddd24d1df2293e4244d3e90
maci993 <savicmarina993@gmail.com>
2025-08-30 08:20:13 +0200

    "commit changes"

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import OurServices from "../../components/OurServices/OurServices";
 
 const Services = () => {
-  return <div></div>;
+  return <div>
+    <OurServices />
+  </div>;
 };
 
 export default Services;
